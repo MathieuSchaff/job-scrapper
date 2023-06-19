ca988e69408c86bdac205f6c33fd44cc70b1ceed
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-06-19 08:31:05 +0200

    the index file will be where all the scrapping func will be called

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
-const { wttjglScrapper } = require("./scrapper/wttjglScrapper");
-const { linkedinScrapper } = require("./scrapper/linkedinScrapper");
-const { indeedScrapper } = require("./scrapper/indeedScrapper");
-const { config } = require("./config/config");
+// const { wttjglScrapper } = require("./wttjgl/main");
+// const { linkedinScrapper } = require("./linkedin/main.js");
+// // const { indeedScrapper } = require("./app/indeedScrapper");
+// const config = require("../config/config.js");
+//
+// wttjglScrapper(config);
