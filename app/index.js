f64070eb4b4014b2119464a23ea173a1ba75ba1a
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-06-19 08:52:39 +0200

    importing and using welcome to the jungle scrapper here

--- app/index.js
+++ app/index.js
@@ -1,6 +1,2 @@
-// const { wttjglScrapper } = require("./wttjgl/main");
-// const { linkedinScrapper } = require("./linkedin/main.js");
-// // const { indeedScrapper } = require("./app/indeedScrapper");
-// const config = require("../config/config.js");
-//
-// wttjglScrapper(config);
+import { wttjglScrapper } from "./wttjgl/main.js";
+wttjglScrapper(config);
